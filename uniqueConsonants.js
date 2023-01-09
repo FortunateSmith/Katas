2b48d3e65662d0d6393cd87de56ad7a423e2fce3
Liam <liam.s.smith@gmail.com>
2023-01-09 10:39:55 -0500

    .

---
+++ uniqueConsonants.js
@@ -0,0 +1,16 @@
+// UNSUCESSFUL 
+
+
+// count the number of unique consonants
+
+function countConsonants(str) {
+  let counter = new Set(str.toLowerCase().replace(/[^a-z][a,e,i,o,u]/g,"")).size
+  console.log(counter);
+  return counter
+  }
+
+
+  return new Set(str.toLowerCase().replace(/[^a-z]|[aeiou]/g,"")).size
+
+  // console.log(countConsonants("sillystring"));
+  countConsonants("sillyString")
